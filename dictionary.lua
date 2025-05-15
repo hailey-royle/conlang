f6f0b6cef1f95121007504d697dfb35c2cbdc6ee
hailey-royle <hailey.royle@proton.me>
2025-05-15 18:01:21 -0400

    dictionary.lua now imports dictionary from dictionary.txt, changes some variable names and made them local

--- dictionary.lua
+++ dictionary.lua
@@ -1,14 +1,24 @@
 print("dictionary file manipulator")
 
---the dictionary
---TODO make the dictionary read from a file
---the last value needs to be a z because the deffinition adder has a stroke if it needs to add at the end of the array, so the z means it always has something after
---TODO get rid of the fucking z its so ugly
-Def_table = { "aaa", "bbb", "z" }
+--get dictionary from dictionary.txt and put it into def_table
+local def_table = {}
+local def_raw = io.open("dictionary.txt", "r")
+if def_raw ~= nil then
+	local incremter = 1
+	local write_def = def_raw:read("*l")
+	while write_def ~= nil do
+		def_table[incremter] = write_def
+		incremter = incremter + 1
+		write_def = def_raw:read("*l")
+	end
+	def_raw:close()
+else
+	print("file not found")
+end
 
 --definition to be added to the dictionary
 --TODO make this be a actual user input
-Def_input = "ccc"
+local def_input = "ccc"
 
 --add the new definition to the place before the first "bigger" definition(alphabetical order)
 --inc_down is incrementing what definiton is checked
@@ -16,18 +26,19 @@ local inc_down = 1
 --inc_in is incremeting what letter is checked
 local inc_in = 1
 repeat
-	if string.sub(Def_input, inc_in, inc_in) > string.sub(Def_table[inc_down], inc_in, inc_in) then
+	if string.sub(def_input, inc_in, inc_in) > string.sub(def_table[inc_down], inc_in, inc_in) then
 		inc_down = inc_down + 1
 		inc_in = 1
-	elseif string.sub(Def_input, inc_in, inc_in) == string.sub(Def_table[inc_down], inc_in, inc_in) then
+	elseif string.sub(def_input, inc_in, inc_in) == string.sub(def_table[inc_down], inc_in, inc_in) then
 		inc_in = inc_in + 1
 	else
-		table.insert(Def_table, inc_down, Def_input)
+		table.insert(def_table, inc_down, def_input)
 		break
 	end
 until nil ~= nil
 
 --temporary output
-print(Def_table[1])
-print(Def_table[2])
-print(Def_table[3])
+print(def_table[1])
+print(def_table[2])
+print(def_table[3])
+print(def_table[4])
