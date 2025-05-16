fb36a12110166ea1156815ceab778cb7b2f5e14e
hailey-royle <hailey.royle@proton.me>
2025-05-16 11:08:01 -0400

    dictionary.lua connected adding a definition to user input

--- dictionary.lua
+++ dictionary.lua
@@ -1,5 +1,7 @@
 print("conlang dictionary file manipulator")
 
+local def_input = nil
+
 --get the user's instructions
 io.write("conlang search(c), english search(e), add definition(a), quit(q): ")
 local instructions = io.read()
@@ -8,7 +10,8 @@ if instructions == "c" then
 elseif instructions == "e" then
 	print("english")
 elseif instructions == "a" then
-	print("add")
+	io.write("definition to put in dictionary: ")
+	def_input = io.read()
 elseif instructions == "q" then
 	print("quit")
 else
@@ -20,27 +23,24 @@ local def_table = {}
 for line in io.lines("dictionary.txt") do
 	table.insert(def_table, line)
 end
-
---user definition to be added to the dictionary
-io.write("definition to put in dictionary: ")
-local def_input = io.read()
-
---add the new definition to the place before the first "bigger" definition(alphabetical order)
---inc_down is incrementing what definiton is checked
-local inc_down = 1
---inc_in is incremeting what letter is checked
-local inc_in = 1
-repeat
-	if string.sub(def_input, inc_in, inc_in) > string.sub(def_table[inc_down], inc_in, inc_in) then
-		inc_down = inc_down + 1
-		inc_in = 1
-	elseif string.sub(def_input, inc_in, inc_in) == string.sub(def_table[inc_down], inc_in, inc_in) then
-		inc_in = inc_in + 1
-	else
-		table.insert(def_table, inc_down, def_input)
-		break
-	end
-until nil ~= nil
+if instructions == "a" then
+	--add the new definition to the place before the first "bigger" definition(alphabetical order)
+	--inc_down is incrementing what definiton is checked
+	local inc_down = 1
+	--inc_in is incremeting what letter is checked
+	local inc_in = 1
+	repeat
+		if string.sub(def_input, inc_in, inc_in) > string.sub(def_table[inc_down], inc_in, inc_in) then
+			inc_down = inc_down + 1
+			inc_in = 1
+		elseif string.sub(def_input, inc_in, inc_in) == string.sub(def_table[inc_down], inc_in, inc_in) then
+			inc_in = inc_in + 1
+		else
+			table.insert(def_table, inc_down, def_input)
+			break
+		end
+	until nil ~= nil
+end
 
 --add def_table to temp file, clones temp file to file, deletes temp file
 io.output("temp_dictionary.txt")
