61b7abcc44612afa576e9a296ef3a6a232b60364
hailey-royle <hailey.royle@proton.me>
2025-09-21 09:40:31 -0600

    emu init

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 if [ "$1" == "asm" ]; then
-    gcc assembler/main.c -Wall -Wextra -std=c99 -Og && ./a.out $2 && rm a.out
+    gcc assembler/main.c -Wall -Wextra -std=c99 -Og 
+    ./a.out $2 
+    rm a.out
+elif [ "$1" == "emu" ]; then
+    gcc emulator/main.c -Wall -Wextra -std=c99 -Og 
+    ./a.out $2 
+    rm a.out
 fi
