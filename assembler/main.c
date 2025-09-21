55fcecf6b80e0aa4116bc828dd366dea035e27b8
hailey-royle <hailey.royle@proton.me>
2025-09-20 18:45:30 -0600

    arg error

--- assembler/main.c
+++ assembler/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("wrong number of args\n");
+        return 1;
+    }
     printf("%d, %s\n", argc, argv[1]);
     return 0;
 }
